2a175392b2e68d649d9b1d9f97026f2f77b28421
Milos Miric <milos.miric@endava.com>
2023-03-15 14:44:01 +0100

    validators folder

---
+++ Models/Validators/ProductViewModelValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Models.ViewModels;
+
+namespace Models.Validators
+{
+    public class ProductViewModelValidator 
+        : AbstractValidator<ProductViewModel>
+    {
+        public ProductViewModelValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotEmpty();
+
+            RuleFor(product => product.Category)
+                .NotEmpty();
+
+            RuleFor(product => product.Price)
+                .NotEmpty()
+                .InclusiveBetween(0, decimal.MaxValue);
+
+            RuleFor(product => product.Description)
+                .Matches("^(\\w|\\s)*$")
+                .MaximumLength(1024);
+        }
+    }
+}
