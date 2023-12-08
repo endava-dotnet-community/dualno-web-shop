0ca8d9a98441a35b95fe648aa437451592ffc997
Petar Vincic <petarvincic005@gmail.com>
2023-12-08 15:59:55 +0100

    GetBySessionId

--- DatabaseEF/Entities/WebshopContext.cs
+++ DatabaseEF/Entities/WebshopContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using DatabaseEF.Entities;
 
 namespace WebShop.DatabaseEF.Entities;
 
@@ -20,6 +21,10 @@ public partial class WebshopContext : IdentityDbContext<IdentityUser>
     public virtual DbSet<CategoryEntity> Categories { get; set; }
 
     public virtual DbSet<ProductEntity> Products { get; set; }
+
+    public virtual DbSet<ShoppingCartItemEntity> ShoppingCartItems { get; set; }
+
+    public virtual DbSet<ShoppingCartEntity> ShoppingCarts { get; set; }
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
--- DatabaseEF/Repositories/ShoppingCartRepository.cs
+++ DatabaseEF/Repositories/ShoppingCartRepository.cs
@@ -1,10 +1,18 @@
 ﻿using Core.Abstractions.Repositories;
+using DatabaseEF.Entities;
 using Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using WebShop.DatabaseEF.Entities;
 
 namespace DatabaseEF.Repositories
 {
     public class ShoppingCartRepository : IShoppingCartRepository
     {
+        readonly WebshopContext _context;
+        public ShoppingCartRepository(WebshopContext dbContext)
+        {
+            _context = dbContext;
+        }
         public Task<bool> DeleteShoppingCartAsync(long cartId)
         {
             throw new NotImplementedException();
@@ -20,9 +28,10 @@ namespace DatabaseEF.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<ShoppingCart> GetBySessionIdAsync(string sessionId)
+        public async Task<ShoppingCart> GetBySessionIdAsync(string sessionId)
         {
-            throw new NotImplementedException();
+            ShoppingCartEntity entity = (await _context.ShoppingCarts.FindAsync(sessionId));
+            return MapFromEntity(entity);
         }
 
         public Task<bool> InsertShoppingCartAsync(ShoppingCart shoppingCart)
@@ -44,5 +53,32 @@ namespace DatabaseEF.Repositories
         {
             throw new NotImplementedException();
         }
+        private static ShoppingCartItem MapFromEntityItems (ShoppingCartItemEntity p)
+        {
+            if (p == null)
+                return null;
+            return new ShoppingCartItem
+            {
+                Id = p.Id,
+                ProductId = p.Product.Id,
+                Quantity = p.Quantity,
+            };
+        }
+        private static ShoppingCart MapFromEntity(ShoppingCartEntity p)
+        {
+            if (p == null)
+                return null;
+
+            return new ShoppingCart
+            {
+            
+                Id = p.Id,
+                AccessedAt = p.AccessedAt,
+                SessionId = p.SessionId,
+                Items = p.Items.Select(i =>MapFromEntityItems(i)).ToList(),
+                    
+            };
+        }
+
     }
 }
