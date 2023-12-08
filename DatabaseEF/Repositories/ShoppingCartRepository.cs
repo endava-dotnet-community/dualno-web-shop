6cadf8c5e98822dd7d827c9b9c95eb73e0d8175b
Veljko <veljkostojanovicima@gmail.com>
2023-12-08 15:53:27 +0100

    Delete Shopping Cart

--- DatabaseEF/Repositories/ShoppingCartRepository.cs
+++ DatabaseEF/Repositories/ShoppingCartRepository.cs
@@ -1,46 +1,61 @@
 ﻿using Core.Abstractions.Repositories;
+using DatabaseEF.Entities;
 using Domain;
+using WebShop.DatabaseEF.Entities;
 
 namespace DatabaseEF.Repositories
 {
     public class ShoppingCartRepository : IShoppingCartRepository
     {
-        public Task<bool> DeleteShoppingCartAsync(long cartId)
+        readonly WebshopContext _context;
+        public ShoppingCartRepository(WebshopContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public async Task<bool> DeleteShoppingCartAsync(long cartId) // ja
+        {
+            ShoppingCartEntity entity = await _context.Carts.FindAsync(cartId);
+
+            if (entity == null)
+                return false;
+
+            _context.Carts.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteShoppingCartItemAsync(long cartItemId) // david
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> DeleteShoppingCartItemAsync(long cartItemId)
+        public async Task<List<ShoppingCart>> GetAllShoppingCartsAsync()// lara
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<ShoppingCart>> GetAllShoppingCartsAsync()
+        public async Task<ShoppingCart> GetBySessionIdAsync(string sessionId)// lara
         {
             throw new NotImplementedException();
         }
 
-        public Task<ShoppingCart> GetBySessionIdAsync(string sessionId)
+        public async Task<bool> InsertShoppingCartAsync(ShoppingCart shoppingCart) // mladen
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> InsertShoppingCartAsync(ShoppingCart shoppingCart)
+        public async Task<bool> InsertShoppingCartItemAsync(ShoppingCartItem shoppingCartItem) // ilija
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> InsertShoppingCartItemAsync(ShoppingCartItem shoppingCartItem)
+        public async Task<bool> UpdateAccessedAtAsync(long cartId, DateTime accessedAt) // petar
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateAccessedAtAsync(long cartId, DateTime accessedAt)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdateQuantityAsync(long cartItemId, int quantity)
+        public async Task<bool> UpdateQuantityAsync(long cartItemId, int quantity) // nikola
         {
             throw new NotImplementedException();
         }
