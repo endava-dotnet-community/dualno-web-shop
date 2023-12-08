50c5577354224f7ece9bee99a3b98d52085106de
Veljko <veljkostojanovicima@gmail.com>
2023-12-08 14:58:58 +0100

    Not Implemented

---
+++ DatabaseEF/Entities/ShoppingCartEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseEF.Entities
+{
+    public partial class ShoppingCartEntity
+    {
+        public long Id { get; set; }
+        public DateTime AccessedAt { get; set; }
+        public string SessionId { get; set; }
+        public virtual ICollection<ShoppingCartItemEntity> Items { get; } = new List<ShoppingCartItemEntity>();
+    }
+}
---
+++ Domain/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class ShoppingCart
+    {
+        public long Id { get; set; }
+        public DateTime AccessedAt { get; set; }
+        public string SessionId { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+    }
+}
