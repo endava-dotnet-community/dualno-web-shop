b205a63d2675ec0569e94c256996d1148aef2e32
Lara Trivunovic <Lara Trivunovic@DESKTOP-175ROFE>
2023-12-15 14:24:00 +0100

    Get all shopping cards

--- DatabaseEF/Repositories/ShoppingCartRepository.cs
+++ DatabaseEF/Repositories/ShoppingCartRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Abstractions.Repositories;
 using DatabaseEF.Entities;
 using Domain;
+using System.Data.Entity;
 using WebShop.DatabaseEF.Entities;
 
 namespace DatabaseEF.Repositories
@@ -12,7 +13,7 @@ namespace DatabaseEF.Repositories
         {
             _context = dbContext;
         }
-        public async Task<bool> DeleteShoppingCartAsync(long cartId) // ja
+        public async Task<bool> DeleteShoppingCartAsync(long cartId) 
         {
             ShoppingCartEntity entity = await _context.Carts.FindAsync(cartId);
 
@@ -25,37 +26,68 @@ namespace DatabaseEF.Repositories
             return true;
         }
 
-        public async Task<bool> DeleteShoppingCartItemAsync(long cartItemId) // david
+        public async Task<bool> DeleteShoppingCartItemAsync(long cartItemId) 
         {
             throw new NotImplementedException();
         }
 
-        public async Task<List<ShoppingCart>> GetAllShoppingCartsAsync()// lara
+        public async Task<List<ShoppingCart>> GetAllShoppingCartsAsync()
+        {
+            return _context
+                .Carts
+                .AsNoTracking()
+                .Select(e => MapFromEntity(e))
+                .ToList();
+        }
+
+        private static ShoppingCartItem MapFromEntity(ShoppingCartItemEntity sci)
+        {
+            if (sci == null)
+                return null;
+
+            return new ShoppingCartItem
+            {
+                Id = sci.Id,
+                ProductId = sci.Product.Id,
+                Quantity = sci.Quantity
+            };
+        }
+
+        private static ShoppingCart MapFromEntity(ShoppingCartEntity sc)
+        {
+            if (sc == null)
+                return null;
+
+            return new ShoppingCart
+            {
+                Id = sc.Id,
+                AccessedAt = sc.AccessedAt,
+                SessionId = sc.SessionId,
+                Items = sc.Items.Select(i => MapFromEntity(i)).ToList()
+            };
+        }
+
+        public async Task<ShoppingCart> GetBySessionIdAsync(string sessionId)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<ShoppingCart> GetBySessionIdAsync(string sessionId)// lara
+        public async Task<bool> InsertShoppingCartAsync(ShoppingCart shoppingCart) 
         {
             throw new NotImplementedException();
         }
 
-        public async Task<bool> InsertShoppingCartAsync(ShoppingCart shoppingCart) // mladen
+        public async Task<bool> InsertShoppingCartItemAsync(ShoppingCartItem shoppingCartItem) 
         {
             throw new NotImplementedException();
         }
 
-        public async Task<bool> InsertShoppingCartItemAsync(ShoppingCartItem shoppingCartItem) // ilija
+        public async Task<bool> UpdateAccessedAtAsync(long cartId, DateTime accessedAt) 
         {
             throw new NotImplementedException();
         }
 
-        public async Task<bool> UpdateAccessedAtAsync(long cartId, DateTime accessedAt) // petar
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<bool> UpdateQuantityAsync(long cartItemId, int quantity) // nikola
+        public async Task<bool> UpdateQuantityAsync(long cartItemId, int quantity) 
         {
             throw new NotImplementedException();
         }
