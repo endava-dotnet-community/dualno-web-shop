a4da8fe7a255fa326b5e7b707991f6364488af4d
Veljko <veljkostojanovicima@gmail.com>
2023-12-22 15:49:03 +0100

    Shopping Cart Repository

--- DatabaseEF/Entities/ShoppingCartItemEntity.cs
+++ DatabaseEF/Entities/ShoppingCartItemEntity.cs
@@ -1,10 +1,13 @@
-﻿using WebShop.DatabaseEF.Entities;
+﻿using DatabaseEF.Migrations;
+using WebShop.DatabaseEF.Entities;
 
 namespace DatabaseEF.Entities
 {
     public partial class ShoppingCartItemEntity
     {
         public long Id { get; set; }
+        public long CartId { get; set; }
+        public ShoppingCartEntity Cart { get; set; }
         public ProductEntity Product { get; set; }
         public int Quantity { get; set; }
     }
--- DatabaseEF/Entities/WebshopContext.cs
+++ DatabaseEF/Entities/WebshopContext.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Domain;
 using DatabaseEF.Entities;
+using System.Reflection.Metadata;
 
 namespace WebShop.DatabaseEF.Entities;
 
@@ -22,16 +23,16 @@ public partial class WebshopContext : IdentityDbContext<IdentityUser>
     public virtual DbSet<CategoryEntity> Categories { get; set; }
 
     public virtual DbSet<ProductEntity> Products { get; set; }
-    public virtual DbSet<ShoppingCartEntity> Carts{ get; set; }
+    public virtual DbSet<ShoppingCartEntity> Carts { get; set; }
     public virtual DbSet<ShoppingCartItemEntity> CartItems { get; set; }
 
     //public virtual DbSet<ShoppingCartItemEntity> ShoppingCartItems { get; set; }
 
     //public virtual DbSet<ShoppingCartEntity> ShoppingCarts { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Server=CTSE-GORANZ;Database=Webshop;Trusted_Connection=True;");
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Server=CTSE-GORANZ;Database=Webshop;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -56,6 +57,24 @@ public partial class WebshopContext : IdentityDbContext<IdentityUser>
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Product_Category");
         });
+
+        modelBuilder.Entity<ShoppingCartEntity>(entity =>
+        {
+            entity.ToTable("ShoppingCart");
+
+            entity
+                .HasMany(e => e.Items)
+                .WithOne(e => e.Cart)
+                .HasForeignKey(e => e.CartId)
+                .IsRequired();
+        });
+
+        modelBuilder.Entity<ShoppingCartItemEntity>(entity =>
+        {
+            entity.ToTable("ShoppingCartItem");
+
+        });
+
 
         OnModelCreatingPartial(modelBuilder);
     }
